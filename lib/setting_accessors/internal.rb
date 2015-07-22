5c99a573970978aa1abc311a829ea1c5b48a23b3
Stefan Exner <stex@sterex.de>
2015-07-23 00:17:06 +0200

    Bumped version to 0.0.6

--- lib/setting_accessors/internal.rb
+++ lib/setting_accessors/internal.rb
@@ -119,7 +119,7 @@ module SettingAccessors
     # @return [Array<String>] all setting accessor names defined in the given +class+
     #
     def self.setting_accessor_names(klass)
-      self.lookup_nested_hash(@@setting_accessor_names, klass.to_s)
+      self.lookup_nested_hash(@@setting_accessor_names, klass.to_s) || []
     end
 
   end
