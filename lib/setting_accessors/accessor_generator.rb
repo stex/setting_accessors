2848ebf044a1c5e456eee8e787839b52fb41e840
Stefan Exner <stex@sterex.de>
2018-11-26 16:09:52 +0100

    #define_method is private up until ruby 2.5

--- lib/setting_accessors/accessor_generator.rb
+++ lib/setting_accessors/accessor_generator.rb
@@ -24,35 +24,42 @@ module SettingAccessors
     private
 
     def define_getters(klass, outer_setting_name = setting_name)
-      klass.define_method(setting_name) do
-        settings.get_or_default(outer_setting_name)
-      end
-
-      # Getter alias for boolean settings
       setting_type = SettingAccessors::Internal.setting_value_type(setting_name, klass).to_sym
-      klass.alias_method "#{outer_setting_name}?", outer_setting_name if setting_type == :boolean
+
+      klass.class_eval do
+        define_method(outer_setting_name) do
+          settings.get_or_default(outer_setting_name)
+        end
+
+        # Getter alias for boolean settings
+        alias_method "#{outer_setting_name}?", outer_setting_name if setting_type == :boolean
+      end
     end
 
     def define_setter(klass, outer_setting_name = setting_name)
-      klass.define_method("#{setting_name}=") do |new_value|
-        settings[outer_setting_name] = new_value
+      klass.class_eval do
+        define_method("#{outer_setting_name}=") do |new_value|
+          settings[outer_setting_name] = new_value
+        end
       end
     end
 
     def define_active_record_helpers(klass, outer_setting_name = setting_name)
-      # NAME_was
-      klass.define_method("#{setting_name}_was") do
-        settings.value_was(outer_setting_name)
-      end
+      klass.class_eval do
+        # NAME_was
+        define_method("#{outer_setting_name}_was") do
+          settings.value_was(outer_setting_name)
+        end
 
-      # NAME_before_type_cast
-      klass.define_method("#{setting_name}_before_type_cast") do
-        settings.value_before_type_cast(outer_setting_name)
-      end
+        # NAME_before_type_cast
+        define_method("#{outer_setting_name}_before_type_cast") do
+          settings.value_before_type_cast(outer_setting_name)
+        end
 
-      # NAME_changed?
-      klass.define_method("#{setting_name}_changed?") do
-        settings.value_changed?(outer_setting_name)
+        # NAME_changed?
+        define_method("#{outer_setting_name}_changed?") do
+          settings.value_changed?(outer_setting_name)
+        end
       end
     end
   end
