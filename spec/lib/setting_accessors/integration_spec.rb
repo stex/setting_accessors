5600f1cec3f2102a48470aeaa2af3fe7ae71acb1
Stefan Exner <stex@sterex.de>
2019-06-03 23:23:46 +0200

    Use generated files instead of dynamic ones in tests
    The changes in this commit should ensure that the generated model and
    migration file are working for each tested Rails version.
    
    To ensure this, the tests now:
    
    * Run the install generator before the test suite
    * Create an actual settings table using the built migration
    * Run each test against the "real" Setting model
    
    This required a few changes to both model and migration template.

--- spec/lib/setting_accessors/integration_spec.rb
+++ spec/lib/setting_accessors/integration_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 describe SettingAccessors::Integration, type: :model do
-  include SettingModel
 
   #----------------------------------------------------------------
   #                           #as_json
--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module Helpers
+  def self.min_ar_version(version)
+    Gem.loaded_specs['activerecord'].version >= Gem::Version.create(version)
+  end
+
   def setting_names(klass)
     SettingAccessors::Internal.setting_accessor_names(klass)
   end
